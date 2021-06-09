5a4bb1f84313187b39db7462c96d1473dc7e9253
GiovanyFT <giovanyfrossard@gmail.com>
2021-06-09 15:38:46 -0300

    Criação do projeto

---
+++ src/main/java/com/servicos/abcde/AbcdeApplication.java
@@ -0,0 +1,13 @@
+package com.servicos.abcde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicos/abcde/AbcdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicos.abcde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
