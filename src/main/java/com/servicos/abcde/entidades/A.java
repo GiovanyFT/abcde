052ae11f2b808c2909b18bb0378d2a4ba828505d
GiovanyFT <giovanyfrossard@gmail.com>
2021-06-10 14:18:09 -0300

    Acrescentando o banco H2

--- src/main/java/com/servicos/abcde/entidades/A.java
+++ src/main/java/com/servicos/abcde/entidades/A.java
@@ -3,13 +3,28 @@ package com.servicos.abcde.entidades;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "tb_a")
 public class A implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String atributo1;
 	private Double atributo2;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant atributo3;
+	
 	private Integer atributo4;
 	
 	public A() {}
