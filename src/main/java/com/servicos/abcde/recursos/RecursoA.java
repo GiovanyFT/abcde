d52b70f454f29f9a98ed4d9ad3f5981f4aad105b
GiovanyFT <giovanyfrossard@gmail.com>
2021-06-17 09:56:42 -0300

    Atualização e Exclusão

--- src/main/java/com/servicos/abcde/recursos/RecursoA.java
+++ src/main/java/com/servicos/abcde/recursos/RecursoA.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +43,18 @@ public class RecursoA {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
 				path("/{id}").buildAndExpand(a.getId()).toUri();
 		return ResponseEntity.created(uri).body(a);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id){
+		servico.excluir(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<A> update(@PathVariable Long id, @RequestBody A obj){
+		obj = servico.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
 
--- src/main/java/com/servicos/abcde/servicos/ServicoA.java
+++ src/main/java/com/servicos/abcde/servicos/ServicoA.java
@@ -1,11 +1,13 @@
 package com.servicos.abcde.servicos;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.servicos.abcde.entidades.A;
 import com.servicos.abcde.repositorios.RepositorioA;
 
@@ -27,6 +29,23 @@ public class ServicoA {
 	
 	public A inserir(A a) {
 		return repositorio.save(a);
+	}
+	
+	public void excluir(Long id) {
+		repositorio.deleteById(id);
+	}
+	
+	public A update(Long id, A objeto_alterado) {
+		A a = repositorio.getById(id);
+		atualizarDados(a, objeto_alterado);
+		return repositorio.save(a);	
+	}
+	
+	public void atualizarDados(A destino, A origem) {
+		destino.setAtributo1(origem.getAtributo1());
+		destino.setAtributo2(origem.getAtributo2());
+		destino.setAtributo3(origem.getAtributo3());
+		destino.setAtributo4(origem.getAtributo4());
 	}
 }
 
